3e55d67ec3c08f888083f30c5b82f1683f21431d
Danubise <danubise@gmail.com>
2017-10-30 17:05:59 +0400

    Added functionality fom read login and password values from db.

--- application/controllers/admin/hlrrequest.php
+++ application/controllers/admin/hlrrequest.php
@@ -32,10 +32,8 @@ class Hlrrequest extends Core_controller {
             foreach($property as $index=>$keyValueArray){
                 $prop[$keyValueArray['propery']] = $keyValueArray['value'];
             }
-            printarray($property);
-            printarray($prop);
 
-            $url = "http://hlr.lanck.alarislabs.com/hlr.cgi?login=login&password=password&dnis=".$parametr['number'] ;
+            $url = "http://hlr.lanck.alarislabs.com/hlr.cgi?login=".$prop['login']."&password=".$prop['password']."&dnis=".$parametr['number'] ;
             $editedkeys=$this->db->select("* from `hlr_keys` ORDER BY `id` ASC");
             //$jsonString = file_get_contents($url);
             $jsonString = "{\"destination\":\"79811613861\",\"id\":\"xxta3hqi62n9urumdmzm\",\"stat\":\"DELIVRD\",\"IMSI\":\"250010027494861\",\"err\":\"0\",\"orn\":\"MTS (Mobile TeleSystems)\",\"pon\":\"MTS (Mobile TeleSystems)\",\"ron\":\"MTS (Mobile TeleSystems)\",\"roc\":\"RU\",\"mccmnc\":\"25001\",\"rcn\":\"Russian Federation\",\"ppm\":\"20\",\"onp\":\"9811\",\"ocn\":\"Russian Federation\",\"occ\":\"RU\",\"ocp\":\"7\",\"is_ported\":\"false\",\"rnp\":\"916\",\"rcp\":\"7\",\"is_roaming\":\"false\",\"pnp\":\"9145\",\"pcn\":\"Russian Federation\",\"pcp\":\"7\",\"pcc\":\"RU\"}";
