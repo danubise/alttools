649dd62e5159f42df30245ae8db3310e269d112f
Danubise <danubise@gmail.com>
2017-10-30 19:55:18 +0400

    made join tables

--- application/controllers/admin/hlrrequest.php
+++ application/controllers/admin/hlrrequest.php
@@ -44,6 +44,7 @@ class Hlrrequest extends Core_controller {
             //$jsonString = file_get_contents($url);
             $jsonString = "{\"destination\":\"79811613861\",\"id\":\"xxta3hqi62n9urumdmzm\",\"stat\":\"DELIVRD\",\"IMSI\":\"250010027494861\",\"err\":\"0\",\"orn\":\"MTS (Mobile TeleSystems)\",\"pon\":\"MTS (Mobile TeleSystems)\",\"ron\":\"MTS (Mobile TeleSystems)\",\"roc\":\"RU\",\"mccmnc\":\"25001\",\"rcn\":\"Russian Federation\",\"ppm\":\"20\",\"onp\":\"9811\",\"ocn\":\"Russian Federation\",\"occ\":\"RU\",\"ocp\":\"7\",\"is_ported\":\"false\",\"rnp\":\"916\",\"rcp\":\"7\",\"is_roaming\":\"false\",\"pnp\":\"9145\",\"pcn\":\"Russian Federation\",\"pcp\":\"7\",\"pcc\":\"RU\"}";
             $arrayResponse = json_decode($jsonString , true);
+            $this->db->delete(" from `hlr_temp`");
             foreach($arrayResponse as $key=>$value){
                 $this->db->insert('hlr_temp', array("original"=>$key, "value"=>$value));
             }
@@ -80,7 +81,9 @@ class Hlrrequest extends Core_controller {
     }
 
     public function editFieldName(){
-// SELECT * FROM `hlr_temp` as hlrt INNER JOIN `hlr_keys`  as hlrk
+//SELECT hlrt.original, hlrk.edited, hlrt.value FROM `hlr_temp` as hlrt LEFT JOIN `hlr_keys`  as hlrk USING(`original`)
+        $joinTables = $this->db->select("hlrt.original, hlrk.edited, hlrt.value FROM `hlr_temp` as hlrt LEFT JOIN `hlr_keys`  as hlrk USING(`original`)",true);
+        printarray($joinTables);
     }
 
     public function logout() {
