ebba2ab799ad8a49e92f4dbc35545dc7a48e1894
Danubise <danubise@gmail.com>
2017-11-07 22:53:46 +0400

    Added time interval

--- application/controllers/admin/miscall.php
+++ application/controllers/admin/miscall.php
@@ -7,90 +7,7 @@ class Miscall extends Core_controller {
     }
 
     public function index() {
-    $date = new DateTime();
-    echo $date->getTimestamp();
-    global $_config_CDR;
-    $cdrdb = new db(
-        $_config_CDR['mysql']['host'],
-        $_config_CDR['mysql']['user'],
-        $_config_CDR['mysql']['password'],
-        $_config_CDR['mysql']['base']
-    );
-    $cdrdb->set_charset("utf8");
-    $allCdrRecodrs = $cdrdb->select("src , calldate, did  FROM cdr WHERE calldate>'2017-11-07 11:00:52' AND calldate<'2017-11-07 12:00:52' AND  disposition = 'NO ANSWER' limit 360");
-
-    $lastMiscallCDR = array();
-    foreach($allCdrRecodrs as $key=>$valueArray){
-
-        if(strlen($valueArray['src'])>4){
-            $src = $valueArray['src'];
-            $src[0] ="8";
-            $lastMiscallCDR[$src]=$valueArray;
-        }
-    }
-
-    $allCdrRecodrs = $cdrdb->select("src , calldate  FROM cdr WHERE calldate>'2017-11-07 11:00:52' AND calldate<'2017-11-07 12:00:52'  AND  disposition = 'ANSWERED' AND billsec>4 limit 360");
-
-    $lastAnsweredcallCDR = array();
-    foreach($allCdrRecodrs as $key=>$valueArray){
-         if(strlen($valueArray['src'])>4){
-             $src = $valueArray['src'];
-             $src[0] ="8";
-            $lastAnsweredcallCDR[$src]=$valueArray['calldate'];
-         }
-    }
-    $allCdrRecodrs = $cdrdb->select("dst , calldate FROM cdr WHERE  calldate>'2017-11-07 11:00:52' AND calldate<'2017-11-07 12:00:52' AND disposition = 'ANSWERED' AND billsec>4 limit 360");
-
-    $lastDialAnsweredcallCDR = array();
-    foreach($allCdrRecodrs as $key=>$valueArray){
-         if(strlen($valueArray['dst'])>4){
-              $src = $valueArray['dst'];
-              $src[0] ="8";
-            $lastDialAnsweredcallCDR[$src]=$valueArray['calldate'];
-         }
-    }
-
-    foreach($lastMiscallCDR as $src=>$valueArray){
-        if($lastAnsweredcallCDR[$src]> $valueArray['calldate']){
-            unset($lastMiscallCDR[$src]);
-        }
-    }
-
-    foreach($lastMiscallCDR as $src=>$valueArray){
-        if($lastDialAnsweredcallCDR[$src]> $valueArray['calldate']){
-            unset($lastMiscallCDR[$src]);
-        }
-    }
-
-    foreach($lastMiscallCDR as $src=>$valueArray){
-        if($valueArray['did'] == "777705") $lastMiscallCDR[$src]['did']="PK krim";
-        if($valueArray['did'] == "777708") $lastMiscallCDR[$src]['did']="PK krnd";
-        if($valueArray['did'] == "79280390600") $lastMiscallCDR[$src]['did']="PK krnd";
-        if($valueArray['did'] == "79263123712") $lastMiscallCDR[$src]['did']="PK msk";
-        if($valueArray['did'] == "777706" || $valueArray['did'] == "777707") $lastMiscallCDR[$src]['did']="PK rostov";
-        if($valueArray['did'] == "777729" || $valueArray['did'] == "777730") $lastMiscallCDR[$src]['did']="Ross-Biz";
-        if($valueArray['did'] == "777727" || $valueArray['did'] == "777728") $lastMiscallCDR[$src]['did']="Rostov-holod";
-        if($valueArray['did'] == "777701" ) $lastMiscallCDR[$src]['did']="PK sochi";
-        if($valueArray['did'] == "79282427127" ) $lastMiscallCDR[$src]['did']="PK sochi";
-        if($valueArray['did'] == "777702" ) $lastMiscallCDR[$src]['did']="PK stavropol";
-        if($valueArray['did'] == "777702" ) $lastMiscallCDR[$src]['did']="PK stavropol";
-        if($valueArray['did'] >= "777731" && $valueArray['did'] <= "777735") $lastMiscallCDR[$src]['did']="KOMPLEKT krd";
-        if($valueArray['did'] == "777704" ) $lastMiscallCDR[$src]['did']="KOMPLEKT krim";
-        if($valueArray['did'] == "79282073771" ) $lastMiscallCDR[$src]['did']="KOMPLEKT krd";
-        if($valueArray['did'] == "79263123727" ) $lastMiscallCDR[$src]['did']="KOMPLEKT msk";
-        if($valueArray['did'] == "79263123727" ) $lastMiscallCDR[$src]['did']="KOMPLEKT msk";
-        if($valueArray['did'] == "777736" ) $lastMiscallCDR[$src]['did']="KOMPLEKT sochi";
-        if($valueArray['did'] == "79282427747" ) $lastMiscallCDR[$src]['did']="KOMPLEKT sochi";
-        if($valueArray['did'] == "777703" ) $lastMiscallCDR[$src]['did']="KOMPLEKT stav";
-        if($valueArray['did'] == "777703" ) $lastMiscallCDR[$src]['did']="KOMPLEKT stav";
-        if($valueArray['did'] == "79281113070" ) $lastMiscallCDR[$src]['did']="KOMPLEKT-UG rst";
-        if($valueArray['did'][0] == "7" &&
-            $valueArray['did'][1] ==  "7" &&
-            $valueArray['did'][2] == "0" &&
-            $valueArray['did'][3] !== "") $lastMiscallCDR[$src]['did']="KOMPLEKT-UG rst";
-        if($valueArray['did'] >= "777721" && $valueArray['did'] <= "777726") $lastMiscallCDR[$src]['did']="KOMPLEKT-UG";
-
-    }
+    $lastMiscallCDR = getMiscallReport();
     $this->view(
             array(
                 'view' => 'miscall/make',
@@ -110,7 +27,15 @@ class Miscall extends Core_controller {
         );
 
     }
-
+    public function send() {
+        sendMiscallReport();
+        $this->view(
+            array(
+                'view' => 'miscall/make',
+                'var' => array()
+            )
+        );
+    }
     public function logout() {
         $this->user_model->logout();
         header('Location: '.baseurl());
--- system/Functions.php
+++ system/Functions.php
@@ -5,6 +5,8 @@
  * Date: 19.02.15
  * Time: 22:14
  */
+require 'PHPMailer_5.2.0/class.phpmailer.php';
+require 'PHPMailer_5.2.0/class.smtp.php';
 
 function printarray($out) {
     echo"<pre>";
@@ -137,4 +139,130 @@ function logger($data,$id=""){
     $data=str_replace("\n","\n".$head,$data);
     $data=trim($data)."\n";
     file_put_contents($file, $data, FILE_APPEND );
+}
+function sendMiscallReport(){
+    $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+
+    //Server settings
+    $mail->SMTPDebug = 2;
+    $mail->IsSMTP();                                      // set mailer to use SMTP
+    $mail->Host = "smtp.yandex.com";  // specify main and backup server
+    $mail->SMTPAuth = true;     // turn on SMTP authentication
+    $mail->Username = "zvonki.ats";  // SMTP username
+    $mail->Password = "secret"; // SMTP password
+    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 587;                                    // TCP port to connect to
+
+    $mail->From = "zvonki.ats@yandex.ru";
+    $mail->FromName = "zvonki.ats";
+    $mail->AddAddress("danubise@gmail.com", "danubise");
+
+    $mail->WordWrap = 50;                                 // set word wrap to 50 characters
+    $mail->IsHTML(true);                                  // set email format to HTML
+
+    $mail->Subject = "Here is the subject";
+    $mail->Body    = "This is the HTML message body in bold!";
+    $mail->AltBody = "This is the body in plain text for non-HTML mail clients";
+
+    if(!$mail->Send())
+    {
+       echo "Message could not be sent.
+    ";
+       echo "Mailer Error: " . $mail->ErrorInfo;
+       exit;
+    }
+}
+function getMiscallReport(){
+    $date = new DateTime();
+
+    $days4 =  gmdate("Y-m-d H:i:s", $date->getTimestamp() - 3600*24*4);
+    global $_config_CDR;
+    $cdrdb = new db(
+        $_config_CDR['mysql']['host'],
+        $_config_CDR['mysql']['user'],
+        $_config_CDR['mysql']['password'],
+        $_config_CDR['mysql']['base']
+    );
+    $cdrdb->set_charset("utf8");
+    $allCdrRecodrs = $cdrdb->select("src , calldate, did  FROM cdr WHERE calldate>'".$days4."' AND  disposition = 'NO ANSWER'");
+
+    $lastMiscallCDR = array();
+    foreach($allCdrRecodrs as $key=>$valueArray){
+
+        if(strlen($valueArray['src'])>4 && is_numeric($valueArray['src'])){
+            $src = $valueArray['src'];
+            if($src[0] =="7" && strlen($src)==11){
+                $src[0] ="8";
+            }
+            $lastMiscallCDR[$src]=$valueArray;
+        }
+    }
+
+    $allCdrRecodrs = $cdrdb->select("src , calldate  FROM cdr WHERE calldate>'".$days4."'  AND  disposition = 'ANSWERED' AND billsec>4");
+
+    $lastAnsweredcallCDR = array();
+    foreach($allCdrRecodrs as $key=>$valueArray){
+         if(strlen($valueArray['src'])>4){
+             $src = $valueArray['src'];
+            if($src[0] =="7" && strlen($src)==11){
+                $src[0] ="8";
+            }
+            $lastAnsweredcallCDR[$src]=$valueArray['calldate'];
+         }
+    }
+    $allCdrRecodrs = $cdrdb->select("dst , calldate FROM cdr WHERE  calldate>'".$days4."' AND disposition = 'ANSWERED' AND billsec>4 ");
+
+    $lastDialAnsweredcallCDR = array();
+    foreach($allCdrRecodrs as $key=>$valueArray){
+         if(strlen($valueArray['dst'])>4){
+              $src = $valueArray['dst'];
+            if($src[0] =="7" && strlen($src)==11){
+                $src[0] ="8";
+            }
+            $lastDialAnsweredcallCDR[$src]=$valueArray['calldate'];
+         }
+    }
+
+    foreach($lastMiscallCDR as $src=>$valueArray){
+        if($lastAnsweredcallCDR[$src]> $valueArray['calldate']){
+            unset($lastMiscallCDR[$src]);
+        }
+    }
+
+    foreach($lastMiscallCDR as $src=>$valueArray){
+        if($lastDialAnsweredcallCDR[$src]> $valueArray['calldate']){
+            unset($lastMiscallCDR[$src]);
+        }
+    }
+
+    foreach($lastMiscallCDR as $src=>$valueArray){
+        if($valueArray['did'] == "777705") $lastMiscallCDR[$src]['did']="PK krim";
+        if($valueArray['did'] == "777708") $lastMiscallCDR[$src]['did']="PK krnd";
+        if($valueArray['did'] == "79280390600") $lastMiscallCDR[$src]['did']="PK krnd";
+        if($valueArray['did'] == "79263123712") $lastMiscallCDR[$src]['did']="PK msk";
+        if($valueArray['did'] == "777706" || $valueArray['did'] == "777707") $lastMiscallCDR[$src]['did']="PK rostov";
+        if($valueArray['did'] == "777729" || $valueArray['did'] == "777730") $lastMiscallCDR[$src]['did']="Ross-Biz";
+        if($valueArray['did'] == "777727" || $valueArray['did'] == "777728") $lastMiscallCDR[$src]['did']="Rostov-holod";
+        if($valueArray['did'] == "777701" ) $lastMiscallCDR[$src]['did']="PK sochi";
+        if($valueArray['did'] == "79282427127" ) $lastMiscallCDR[$src]['did']="PK sochi";
+        if($valueArray['did'] == "777702" ) $lastMiscallCDR[$src]['did']="PK stavropol";
+        if($valueArray['did'] == "777702" ) $lastMiscallCDR[$src]['did']="PK stavropol";
+        if($valueArray['did'] >= "777731" && $valueArray['did'] <= "777735") $lastMiscallCDR[$src]['did']="KOMPLEKT krd";
+        if($valueArray['did'] == "777704" ) $lastMiscallCDR[$src]['did']="KOMPLEKT krim";
+        if($valueArray['did'] == "79282073771" ) $lastMiscallCDR[$src]['did']="KOMPLEKT krd";
+        if($valueArray['did'] == "79263123727" ) $lastMiscallCDR[$src]['did']="KOMPLEKT msk";
+        if($valueArray['did'] == "79263123727" ) $lastMiscallCDR[$src]['did']="KOMPLEKT msk";
+        if($valueArray['did'] == "777736" ) $lastMiscallCDR[$src]['did']="KOMPLEKT sochi";
+        if($valueArray['did'] == "79282427747" ) $lastMiscallCDR[$src]['did']="KOMPLEKT sochi";
+        if($valueArray['did'] == "777703" ) $lastMiscallCDR[$src]['did']="KOMPLEKT stav";
+        if($valueArray['did'] == "777703" ) $lastMiscallCDR[$src]['did']="KOMPLEKT stav";
+        if($valueArray['did'] == "79281113070" ) $lastMiscallCDR[$src]['did']="KOMPLEKT-UG rst";
+        if($valueArray['did'][0] == "7" &&
+            $valueArray['did'][1] ==  "7" &&
+            $valueArray['did'][2] == "0" &&
+            $valueArray['did'][3] !== "") $lastMiscallCDR[$src]['did']="KOMPLEKT-UG rst";
+        if($valueArray['did'] >= "777721" && $valueArray['did'] <= "777726") $lastMiscallCDR[$src]['did']="KOMPLEKT-UG";
+
+    }
+    return $lastMiscallCDR;
 }
