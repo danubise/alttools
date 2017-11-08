2d6a00c51cd5bb273811a1822fe97e1b421efa11
Danubise <danubise@gmail.com>
2017-11-08 15:48:01 +0400

    Changed equation by unqueid

--- system/Functions.php
+++ system/Functions.php
@@ -199,7 +199,7 @@ function getMiscallReport(){
         $_config_CDR['mysql']['base']
     );
     $cdrdb->set_charset("utf8");
-    $allCdrRecodrs = $cdrdb->select("src , uniqueid, calldate, did  FROM cdr WHERE LIKE  '%9889557998%' AND calldate>'".$days4."' AND  disposition = 'NO ANSWER' ORDER BY `calldate`, `uniqueid` ASC ");
+    $allCdrRecodrs = $cdrdb->select("src , uniqueid, calldate, did  FROM cdr WHERE  `clid` LIKE  '%9889557998%' AND calldate>'".$days4."' AND  disposition = 'NO ANSWER' ORDER BY `calldate`, `uniqueid` ASC ");
 
     $lastMiscallCDR = array();
     foreach($allCdrRecodrs as $key=>$valueArray){
